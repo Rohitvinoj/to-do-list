9a216b6f5b8123c58faf353e9e03a1a832ef3f70
Rohitvinoj <106524839+Rohitvinoj@users.noreply.github.com>
2022-11-23 13:34:21 +0530

    Delete to-do-app-23-11-22 directory

--- to-do-app-23-11-22/todo/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.urls import reverse
-
-class Schedule(models.Model):
-    topic  = models.TextField(max_length = 200)
-
-    def __str__(self):
-        return self.topic
